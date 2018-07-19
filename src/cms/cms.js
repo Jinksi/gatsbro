363845d8d29c2459c1879551865faf5917be0dec
Eric Jinks <ericjinks@gmail.com>
2018-07-19 15:12:05 +1000

    cms-utils

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CMS from 'netlify-cms'
+import './cms-utils'
 
 import { HomePageTemplate } from '../templates/HomePage'
 import { AboutPageTemplate } from '../templates/AboutPage'
@@ -28,33 +29,3 @@ CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
 CMS.registerPreviewTemplate('posts', ({ entry }) => (
   <SinglePostTemplate {...entry.toJS().data} />
 ))
-
-if (typeof window !== 'undefined') {
-  // add admin.css
-  const link = document.createElement('link')
-  link.type = 'text/css'
-  link.rel = 'stylesheet'
-  link.href = '/admin/admin.css'
-  document.head.appendChild(link)
-
-  if (process.env.NETLIFY_SITE_URL) {
-    window.localStorage.setItem('netlifySiteURL', process.env.NETLIFY_SITE_URL)
-  }
-  // Log netlifySiteURL if editing on localhost
-  if (
-    window.location.hostname === 'localhost' &&
-    window.localStorage.getItem('netlifySiteURL')
-  ) {
-    console.log(
-      `%cnetlifySiteURL: ${window.localStorage.getItem('netlifySiteURL')}`,
-      'color: hotpink; font-size: 15px'
-    )
-  }
-
-  // check for netlifyIdentity, redirect to admin if user is logging in
-  if (window.localStorage && window.netlifyIdentity) {
-    netlifyIdentity.on('login', function() {
-      document.location.reload()
-    })
-  }
-}
